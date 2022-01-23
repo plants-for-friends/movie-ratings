1bb68f4def663e33881616b605c54e286dd70e4c
Carlson43 <58775878+Carlson43@users.noreply.github.com>
2022-01-23 12:17:20 +0100

    added custom Favicon

--- src/lib/navbar/Navbar.svelte
+++ src/lib/navbar/Navbar.svelte
@@ -1,38 +1,9 @@
 <script>
-	import { page } from '$app/stores';
+	import { page } from '$app/stores';	//Import current page for page identifier on Navbar
 	import logo from './logo.svg';
 </script>
 
 <header>
-	<!-- <div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
-
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-			<li class:active={$page.url.pathname === '/about'}>
-				<a sveltekit:prefetch href="/about">About</a>
-			</li>
-			<li class:active={$page.url.pathname === '/todos'}>
-				<a sveltekit:prefetch href="/todos">Test-Name</a>
-			</li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav> -->
-
-	<!-- Spacer, for centering navabr -->
-	<!-- <div class="corner">
-	</div> -->
-
-
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container fluid">
             <!-- Navabr Logo and Brandname -->
