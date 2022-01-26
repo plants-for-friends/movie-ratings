494166f0de250c0c2abe90df42b11024086ecc51
Carlson43 <58775878+Carlson43@users.noreply.github.com>
2022-01-26 10:14:47 +0100

    added head element

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
+<script>
+    // omit script element if it is not needed on your page
+</script>
+<svelte:head>
+<title>my-sveltekit-app | Home Page</title>
+    <html lang="en-GB" />
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
