e3174c47a9b3edeea598b3abd0fb7abd608dd364
Carlson43 <58775878+Carlson43@users.noreply.github.com>
2022-02-14 10:23:50 +0100

    added Todo-List

--- my-todo-app/src/routes/index.svelte
+++ my-todo-app/src/routes/index.svelte
@@ -1,6 +1,7 @@
 <script>
-    import { text } from "svelte/internal";
+    import {todos} from "../stores/todoStore";
     import TodoForm from "../components/TodoForm.svelte";
+    import Todo from "../components/Todo.svelte";
 </script>
 
 
@@ -8,4 +9,7 @@
 <main>
     <h1 class="text-2xl font-bold text-center text-gray-800 md.text-3xl">My Todos</h1>
     <TodoForm/>
+    {#each $todos as todo}
+        <Todo todo={todo} index={todos.id}/>
+    {/each}
 </main>
