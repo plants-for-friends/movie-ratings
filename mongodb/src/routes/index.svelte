d7c616521340595cb521a86d6b30b0a55b924b23
antoine-fuchs <94910891+antoine-fuchs@users.noreply.github.com>
2022-02-19 17:14:58 +0100

    addTodo Added

--- mongodb/src/routes/index.svelte
+++ mongodb/src/routes/index.svelte
@@ -3,7 +3,11 @@
 </script>
 
 <script>
-	// import Counter from '$lib/Counter.svelte';
+	let text
+
+	async function addTodo(){
+		alert(text + 'Todo Added')
+	}
 
 </script>
 
@@ -15,6 +19,8 @@
 
 	<main> 
 		<h1>My Todos</h1>
+		<input type="text" bind:value={text} />
+		<button on:click={addTodo}>Add ToDo</button>
 	</main>
 
 </section>
