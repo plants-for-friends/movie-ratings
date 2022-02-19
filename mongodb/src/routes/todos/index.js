d56bc33058447bc924abe0a53242f7103b56ef51
antoine-fuchs <94910891+antoine-fuchs@users.noreply.github.com>
2022-02-19 16:27:54 +0100

    Connection done

--- mongodb/src/routes/todos/index.js
+++ mongodb/src/routes/todos/index.js
@@ -1,14 +1,26 @@
-
+import { connectToDatabase } from "$lib/db"
+import { ObjectID } from 'mongodb'
 
 export async function get(request){
+	try{
 
-	const completed = request.url.searchParams.get('completed') === 'true' ? true : false
+		const completed = request.url.searchParams.get('completed') === 'true' ? true : false
+		const dbConnection = await connectToDatabase()
+		const db = dbConnection.db
+		const collection = db.collection('Test')
+		const todos = await collection.find({completed}).toArray()
 
 
-	return{
-		status: 200,
-		body:{
-			completed: completed
+		return{
+			status: 200,
+			body:{
+				todos
+			}
+		}
+	} catch(err){
+		return{
+			status: 500,
+			body: {error: 'Server Error'}
 		}
 	}
 }
